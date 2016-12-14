db92b071dc3a5144fce98d9ba34f9733231472d3
Samay Patel <samay@capyx.com>
2016-12-14 14:20:02 -0800

    segment added

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = "5927af48bebd9272eb6276e09ea95008"
+  config.api_key = '5927af48bebd9272eb6276e09ea95008'
+  config.notify_release_stages = 'production'
 end
