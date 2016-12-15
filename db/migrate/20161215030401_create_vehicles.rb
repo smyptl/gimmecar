a7a568f316b3f69fc5ae8219ed0e064f4d40ef58
Samay Patel <samay@capyx.com>
2016-12-14 21:21:06 -0800

    first set of models created

---
+++ db/migrate/20161215030401_create_vehicles.rb
@@ -0,0 +1,21 @@
+class CreateVehicles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :vehicles do |t|
+      t.timestamp
+      t.string :vehicle_type
+      t.string :vin
+      t.string :license_number
+      t.string :make
+      t.string :model
+      t.integer :year
+      t.string :color
+      t.integer :original_odometer
+      t.string :transmission
+      t.string :power_train
+      t.integer :cylinders
+      t.string :fuel_grade
+      t.integer :tank_size
+      t.text :notes
+    end
+  end
+end
