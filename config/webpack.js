b41c29e82c0c1fa4929443ee18fcafaa7be4dfe2
Samay Patel <samay@capyx.com>
2016-12-19 15:12:31 -0800

    webpack init

---
+++ config/webpack.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var projectRoot = path.resolve(__dirname, '../');
+
+module.exports = {
+  entry: './app/frontend/index.js',
+  output: {
+    path: path.resolve(__dirname, '../app/assets/javascripts'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+    ],
+  },
+}
