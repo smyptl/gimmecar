9b42cd456f71796ce35b4f85eaf1c56de9209c62
Samay Patel <smyptl@users.noreply.github.com>
2017-03-17 17:11:11 -0700

    minor improvements

--- app/frontend/components/dropdown.vue
+++ app/frontend/components/dropdown.vue
@@ -90,7 +90,7 @@
       width: 100%
 
       list-style: none
-      background: #ffffff
+      background: $white
 
       border-radius: 0.125rem
       box-shadow: 0 3.125rem 6.25rem rgba(50, 50, 50, .1), 0 0.5rem 2rem rgba(150, 150, 150, 0.15), 0 0.125rem 1rem rgba(0, 0, 0, .1)
