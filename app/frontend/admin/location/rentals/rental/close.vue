1abd236cf03ca0dde8263dfb47f8e531f9596bf9
Samay Patel <samay@capyx.com>
2017-03-24 12:30:51 -0700

    close drop off created

--- app/frontend/admin/location/rentals/rental/close.vue
+++ app/frontend/admin/location/rentals/rental/close.vue
@@ -1,22 +1,35 @@
 <script>
+  import Form from 'Utils/form'
+  import Shake from 'Utils/transitions/shake'
+
+  import InputDateTime from 'Components/inputs/date_time'
+
   import Popup from 'Components/popup'
 
   export default {
     data () {
       return {
         open: false,
+        form: new Form({
+          drop_off_date: new Date(),
+          drop_off_fuel: 10,
+          drop_off_odometer: '',
+        })
       }
     },
     mounted () {
       this.open = true
     },
     components: {
-      Popup
+      Popup,
+      InputDateTime,
     },
     methods: {
       close () {
         this.open = false
-      }
+      },
+      closeRental () {
+      },
     },
   }
 </script>
@@ -24,14 +37,29 @@
 <template lang='pug'>
   popup(v-if='open')
     .panel-popup-form
-      .panel-base-header
-        h2 Close - #12312313
+      .panel-form.panel-form-padding
+        h3.panel-form-header Close - #12312313
 
-      .form-container
-        label.input-label(for='drop_off_date') Drop Off
-        .input-block.whole
-          input.input-field.input-contrast#drop_off_date(type='text')
+        .input-row
+          label.input-label(for='drop_off_date') Drop Off
+          .input-block.whole
+            input-date-time.input-field.input-contrast#drop_off_date(v-model='form.drop_off_date')
 
+        .input-row
+          .input-container.two-fifths
+            label.input-label(for='drop_off_odometer') Vehicle Odometer
+            .input-block.whole
+              input.input-field.input-contrast#drop_off_odometer(type='text' v-model='form.drop_off_odometer')
+
+          .input-container.three-fifths
+            label.input-label(for='drop_off_fuel')
+              | Fuel Level
+              .input-label-note.right {{ form.drop_off_fuel * 10 }}%
+            .input-block.whole
+              input.input-range#drop_off_fuel(type='range' v-model.number='form.drop_off_fuel' min='0' max='10')
+
+        .input-submit.input-block
+          button.btn.btn-primary.right(@click.prevent='closeRental()') Continue
 </template>
 
 <style lang='stylus' scoped>
