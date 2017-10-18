27c56b1e3c3c2026f0e624ff6198a490e11d500f
Samay Patel <samay@capyx.com>
2017-10-18 08:34:48 -0700

    logic rates refactor

--- app/gimmecar/logic/rates.rb
+++ app/gimmecar/logic/rates.rb
@@ -1,13 +1,18 @@
-class Logic::Rates < Lib::Services::Base
+class Logic::Rates
+
+  attr_reader :rental_period, :vehicle_type, :location
 
   def initialize(pickup:, drop_off:, vehicle_type:, location:, tax_calculator: LineItem)
+    @rental_period  = Lib::DateRange.new(pickup, drop_off)
+    @vehicle_type   = vehicle_type
+    @location       = location
+    @tax_calculator = tax_calculator
+  end
+
+  def fetch
   end
 
   private
-
-  def rental_period
-    @rental_period ||= Lib::DateRange.new(rental.pickup, rental.drop_off)
-  end
 
   def rates
   end
