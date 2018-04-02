8b1c294a7e456ab7946ff0eda6a7117e72089c2c
Samay Patel <smyptl@gmail.com>
2018-04-01 21:49:38 -0700

    return deposit tested

--- spec/system/command/rentals/rental/return_deposit_spec.rb
+++ spec/system/command/rentals/rental/return_deposit_spec.rb
@@ -9,10 +9,12 @@ describe 'Return Deposit', type: :system, js: true do
   include_context :login_user
 
   scenario 'success' do
-    amount    = 4000
-    rental    = create(:rental, :closed)
-    charge    = create(:charge, :create_stripe_charge, amount: amount, owner: rental)
-    line_item = create(:line_item, :deposit, total: amount, charge: charge, invoice: rental)
+    deposit_amount = 4000
+    charge_amount  = 2000
+    amount         = deposit_amount + charge_amount
+    rental         = create(:rental, :closed)
+    charge         = create(:charge, :create_stripe_charge, amount: amount, owner: rental)
+    line_item      = create(:line_item, :deposit, total: deposit_amount, charge: charge, invoice: rental)
 
     visit_command command_rental_path(id: rental.number)
 
@@ -29,7 +31,10 @@ describe 'Return Deposit', type: :system, js: true do
 
     expect(Charge.count).to eq(1)
     charge = Charge.first
-    expect(charge.amount).to eq(0)
+    expect(charge.amount).to eq(charge_amount)
+    stripe_charge = Stripe::Charge.retrieve(charge.stripe_charge_id)
+    expect(stripe_charge['amount']).to eq(amount)
+    expect(stripe_charge['amount_refunded']).to eq(deposit_amount)
 
     expect(LineItem.count).to eq(0)
 
