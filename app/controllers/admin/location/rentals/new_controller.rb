54fe7173921807b2c744d70fd95f4373ac345da8
Samay Patel <smyptl@gmail.com>
2018-04-23 11:28:02 -0700

    new controller change in params

--- app/controllers/admin/location/rentals/new_controller.rb
+++ app/controllers/admin/location/rentals/new_controller.rb
@@ -12,7 +12,14 @@ class Admin::Location::Rentals::NewController < Admin::Location::BaseController
   end
 
   def validate_drivers
-    Actions::Admin::Location::Rental::New::ValidateDrivers.new(params).execute(success, failure, location_id: location.id)
+    Actions::Admin::Location::Rental::New::ValidateDrivers.new({
+      drop_off:              params.fetch(:drop_off),
+      driver_id:             params.fetch(:driver_id),
+      driver:                params.fetch(:driver),
+      add_additional_driver: params.fetch(:add_additional_driver),
+      additional_driver_id:  params.fetch(:additional_driver_id),
+      additional_driver:     params.fetch(:additional_driver),
+    }).execute(success, failure, location_id: location.id)
   end
 
   def driver_search
@@ -27,15 +34,29 @@ class Admin::Location::Rentals::NewController < Admin::Location::BaseController
   end
 
   def validate_vehicle
-    Actions::Admin::Location::Rental::New::ValidateVehicle.new(params).execute(success, failure, location_id: location.id )
+    Actions::Admin::Location::Rental::New::ValidateVehicle.new({
+      drop_off:        params.fetch(:drop_off),
+      vehicle_type:    params.fetch(:vehicle_type),
+      vehicle_id:      params.fetch(:vehicle_id),
+      pickup_odometer: params.fetch(:pickup_odometer),
+      pickup_fuel:     params.fetch(:pickup_fuel),
+    }).execute(success, failure, location_id: location.id )
   end
 
   def validate_financial_responsibility
-    Actions::Admin::Location::Rental::New::ValidateFinancialResponsibility.new(params).execute(success, failure)
+    Actions::Admin::Location::Rental::New::ValidateFinancialResponsibility.new({
+      add_additional_driver:                                params.fetch(:add_additional_driver),
+      driver_financial_responsibility_signature:            params.fetch(:driver_financial_responsibility_signature),
+      additional_driver_financial_responsibility_signature: params.fetch(:additional_driver_financial_responsibility_signature),
+    }).execute(success, failure)
   end
 
   def validate_terms_and_conditions
-    Actions::Admin::Location::Rental::New::ValidateTermsAndConditions.new(params).execute(success, failure)
+    Actions::Admin::Location::Rental::New::ValidateTermsAndConditions.new({
+      add_additional_driver:       params.fetch(:add_additional_driver),
+      driver_signature:            params.fetch(:driver_signature),
+      additional_driver_signature: params.fetch(:additional_driver_signature),
+    }).execute(success, failure)
   end
 
   def create
