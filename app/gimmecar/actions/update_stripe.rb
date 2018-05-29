25265ae8739be4d1e4df8dfdc05b956f651082e5
Samay Patel <smyptl@gmail.com>
2018-05-29 15:13:19 -0700

    update stripe bug fix

--- app/gimmecar/actions/update_stripe.rb
+++ app/gimmecar/actions/update_stripe.rb
@@ -4,11 +4,13 @@ class Actions::UpdateStripe < Lib::Actions::Base
     {}
   end
 
+  private
+
   def save
-    Driver.where.not(stripe_id: nil).each do |driver|
+    Driver.where.not(stripe_id: nil).includes(:emails, :email_primary).each do |driver|
       cu             = driver.stripe_customer
       cu.description = driver.name
-      cu.email       = driver.email
+      cu.email       = (driver.email_primary || driver.emails.frist).email
       cu.save
     end
   end
